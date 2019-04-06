3505c468b01d2c9dc140bbf06331db036a586c56
jonathanbenson <46434056+jonathanbenson@users.noreply.github.com>
2019-04-06 12:26:08 -0700

    Hackathon 2019 working

---
+++ src/resources/N_Gram.py
@@ -0,0 +1,20 @@
+
+from Word import Word
+
+class N_Gram :
+
+    def __init__(self , n , words , count = 1) :
+
+        self.n = n
+        self.words = words
+
+        self.count = count
+
+    def inc_count(self) :
+
+        self.count += 1
+
+
+
+
+
--- src/resources/RP.py
+++ src/resources/RP.py
@@ -1,8 +1,8 @@
 
-from Word import RPW , KW
-from CONSTANTS import CWs
+from Word import Word
 import os
-
+from nltk.corpus import stopwords
+from N_Gram import N_Gram
 import PyPDF2
 
 
@@ -17,8 +17,16 @@ class RP :
         #list of words (RPW object)
         self.words = list()
 
-        #list of keywords (KW object)
-        self.keywords = list()
+        self.n_limit = int()
+
+        #list of n_gram objects
+        self.n_grams = list()
+
+        #list of dictionaries
+        self.n_gram_counts = list()
+
+        #list of stopwords
+        self.sws = set(stopwords.words('english'))
 
     def set_file_name(self , file_name) :
         '''checks if the file is in the current working directory and if it is, sets self.file_name equal to that file's name'''
@@ -37,7 +45,6 @@ class RP :
         except ValueError :
 
             print("Error. {} is not in {}".format(file_name , os.getcwd()))
-  
 
     def get_text(self) :
         '''sets self.text equal to a list of the lines (as strings) of the file (self.file_name)'''
@@ -49,6 +56,7 @@ class RP :
         num_pages = pdf_reader.numPages
         count = 0
         text = ""
+
         # The while loop will read each page
         while count < num_pages:
             pageObj = pdf_reader.getPage(count)
@@ -57,80 +65,173 @@ class RP :
 
 
 
-        self.text = text.split("\n")
+        self.text = text
+
+    def get_stop_words(self) :
+
+        with open("SWs.txt" , "r") as reader :
+
+            self.sws = reader.readlines()
+
+        for i , sw in enumerate(self.sws) :
+
+            sw = list(sw)
+            sw.pop(-1)
+
+            self.sws[i] = "".join(sw)
+
+    def display_sws(self) :
+
+        for sw in self.sws :
+
+            print(sw)
 
     def get_words(self) :
         '''parses all the words from the file self.file_name, counts them, and puts them into the list self.words'''
+        '''FIXME'''
 
-        for line in self.text :
+        delimiters = ["\n" , "\t" , "." , "," , "?" , "/"]
 
-            words = line.split(" ")
-            
-            for word in words :
+        for delimiter in delimiters :
 
-                append_new_word = True
+            self.text = self.text.replace(delimiter , " ")
 
-                new_word = RPW(word.lower())
-                new_word._format()
-
-                if len(new_word.chars) > 0 :
-
-                    if len(self.words) > 1 :
-                        
-                        for previous_word in self.words :
-
-                            if new_word.chars == previous_word.chars :
-
-                                previous_word.inc_count()
-
-                                append_new_word = False
+        words = self.text.split(" ")
 
 
-                    if append_new_word == True :
-                    
-                        self.words.append(new_word)
+        for word in words :
 
-    def sort_words_by_count(self) :
-        '''sorts all of the words in self.words by their count'''
+            new_word = Word(word.lower())
+            new_word._format()
 
-        self.words.sort(key = lambda word : word.count , reverse = True)
+            if len(new_word.chars) > 0 :
 
+                self.words.append(new_word)
 
-    def get_keywords(self) :
-        '''removes all the words that occur in CWsfrom self.words and sets it equal to self.keywords'''
+    def set_n_limit(self , n_limit) :
 
-        #zws stands for zipf words (contains the strings of all the zipf words)
-        zws = [zw.chars for zw in CWs]
+        self.n_limit = n_limit
 
-        self.keywords = [KW(word.chars , word.count) for word in self.words]
+    def get_n_grams(self) :
 
-        remove_count = 0
+        def const_lst_n_grams(degree , words):
 
-        for word in self.keywords.copy() :
+            lst_n_grams = list()
 
-            #disqualifying conditions for words (RPW object) to become keywords (KW object)
-            if word.chars in zws \
-                    or len(word.chars) <= 4 :
+            for i in range(len(words)):
 
-                self.keywords.remove(word)
-                remove_count += 1
+                if i <= (len(words) - degree):
+                    lst_n_grams.append(
+                        N_Gram(degree , [words[i + n] for n in range(degree)])
+                    )
 
 
 
-            if remove_count == len(CWs) :
+            return lst_n_grams
+
+        for degree in range(1 , self.n_limit + 1) :
+
+            self.n_grams.append(const_lst_n_grams(degree , self.words))
+
+    def filter_n_grams(self) :
+
+        for degree in range(self.n_limit) :
+
+            for n_gram in self.n_grams[degree].copy() :
+
+                for word in n_gram.words :
+
+                    if word.chars in self.sws :
+
+                        try :
+
+                            self.n_grams[degree].remove(n_gram)
+
+                        except ValueError :
+
+                            is_something_wrong = "no"
+
+    def display_n_grams(self) :
+
+        for degree in range(self.n_limit) :
+
+            for n_gram in self.n_grams[degree][:10] :
+
+                print([word.chars for word in n_gram.words] , n_gram.count , end = " ")
+
+            print()
+
+    def get_n_gram_counts(self) :
+        '''list of dictionaries of containingn_gram_counts by their words'''
+
+        for degree in range(self.n_limit) :
+
+            self.n_gram_counts.append(dict())
+
+            for n_gram in self.n_grams[degree] :
+
+                #",".join([word.chars for word in n_gram.words])
+                try :
+
+                    self.n_gram_counts[degree][",".join([word.chars for word in n_gram.words])] += 1
+
+                except KeyError :
+
+                    self.n_gram_counts[degree][",".join([word.chars for word in n_gram.words])] = 1
+
+    def display_n_gram_counts(self) :
+        ''''''
+
+        for degree in range(self.n_limit) :
+
+            iterations = 0
+
+            for n_gram , count in self.n_gram_counts[degree].items() :
+
+                print(n_gram , count , end = " ")
+
+                iterations += 1
+
+                if iterations == 10 :
+
+                    break
+
+            print()
+
+    def sort_n_grams(self) :
+
+        self.n_grams = list()
+
+        for degree in range(self.n_limit) :
+
+            self.n_grams.append(list())
+
+            for words , count in self.n_gram_counts[degree].items() :
+
+                self.n_grams[degree].append(
+
+                    N_Gram(
+
+                        degree + 1 ,
+
+                        [Word(word) for word in words.split(",")] ,
+
+                        count
+                    )
+
+                )
+
+        for degree in range(self.n_limit) :
+
+            self.n_grams[degree].sort(key = lambda n_gram : n_gram.count , reverse = True)
 
 
-                break
 
-    def display_words(self) :
-        '''Prints to the console self.words'''
 
-        print("Words: {}".format([(self.words[i].chars , self.words[i].count) for i in range(len(self.words))]))
 
-    def display_keywords(self) :
-        '''Prints to the console self.keywords'''
 
-        print("Keywords: {}".format([(self.keywords[i].chars , self.keywords[i].count) for i in range(len(self.keywords))]))
+
+
 
                 
 
